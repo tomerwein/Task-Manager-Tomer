731d1073425653ca3db97406874c54f3bfa462c0
tomer <tomer1w1@gmail.com>
2023-02-23 20:05:14 +0200

    update the main App and add new file of all the things that been use in the main app beforehead

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,12 @@
-import React, { useState } from 'react';
-import './App.css';
-import Input from './components/Input';
-import Task from './taskInfo';
-import TaskList from './components/TaskList';
-import { DragDropContext, DropResult } from 'react-beautiful-dnd';
-
+import React from 'react'
+import TaskManager from './TaskManager'
 
 const App: React.FC = () => {
-  const [task, setTask] = useState<string>("");
-  const [importantTasks, setImportantTasks] = useState<Task[]>([]);
-  const [generalTasks, setGeneralTasks] = useState<Task[]>([]);
-  const [completedTasks, setCompletedTasks] = useState<Task[]>([]);
-  
-  const addToImportantList = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (task){
-      setImportantTasks([...importantTasks, {id: Date.now(), task: task, type: "important", isDone: false}]);
-      setTask("");
-
-    } 
-  }
-
-  const addToGeneralList = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (task){
-      setGeneralTasks([...generalTasks, {id: Date.now(), task: task, type: "general", isDone: false}]);
-      setTask("");
-    } 
-  }
-
-  const onDragEnd = (result: DropResult) => {
-    const {source, destination} = result;
-    if (!destination) return;
-
-    if (
-      source.index === destination.index && 
-      source.droppableId === destination.droppableId
-    )
-      return;
-
-    let add,
-      important = importantTasks,
-      general = generalTasks,
-      complete = completedTasks;
-
-    if (source.droppableId === 'ImportantList'){
-      add = important[source.index];
-      important.splice(source.index, 1);
-    } else if (source.droppableId === 'GeneralList'){
-      add = general[source.index];
-      general.splice(source.index, 1)
-    } else {
-      add = complete[source.index];
-      complete.splice(source.index, 1)
-    }
-
-    if (destination.droppableId === 'ImportantList'){
-      add.type = "important";
-      important.splice(destination.index, 0, add)
-    } else if (destination.droppableId === 'GeneralList'){
-      add.type = "general";
-      general.splice(destination.index, 0, add)
-    } else { 
-      complete.splice(destination.index, 0, add) 
-    }
-
-   setImportantTasks(important); 
-   setGeneralTasks(general);
-   setCompletedTasks(complete);
-  }
-
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <div className="App">
-        <span className="heading"> Task Manager </span>
-        <Input task={task} setTask={setTask} addToImportantList={addToImportantList} addToGeneralList={addToGeneralList} />
-        <TaskList
-          importantTasks={importantTasks}
-          setImportantTasks={setImportantTasks}
-          generalTasks={generalTasks}
-          setGeneralTasks={setGeneralTasks}
-          completedTasks={completedTasks}
-          setCompletedTasks={setCompletedTasks}
-        />
-      </div>
-    </DragDropContext>
-  );
-};
+    <div>
+      <TaskManager/>
+    </div>
+  )
+}
 
-export default App;
+export default App
