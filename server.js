a5f2669adc23910a8d14ebb68aa1f7d5b09fb521
tomer <tomer1w1@gmail.com>
2023-03-09 18:08:18 +0200

    add some dependencies

--- server.js
+++ server.js
@@ -16,32 +16,28 @@ app.post('/register', (req, res) => {
   const { user, password } = req.body;
   console.log(user, password);
 
-  // Read the existing data from the file (if any)
   const existingData = fs.readFileSync('src/data/db.json');
 
-  // Parse the existing data as a JSON object
   const data = JSON.parse(existingData);
   console.log(data);
 
-  // Check if the user already exists
   const userExists = data.register.some((entry) => entry.user === user);
-
   if (userExists) {
     res.status(409).send({message: 'User already exists'});
     return;
   }
 
-  // Add the new user data to the array
   data.register.push({user, password});
 
-  // Write the updated data back to the file
   fs.writeFileSync('src/data/db.json', JSON.stringify(data));
 
   res.status(200).send({message: 'great!'});
-  // req.status(200).send('great!');
+
 })
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+
 
